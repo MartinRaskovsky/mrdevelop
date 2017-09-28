1130e324cf36e4e443717b77adeefb02437b623d
Martin Raskovsky <mail@martinr.com>
2017-09-29 00:31:40 +0100

    Made selection persistent

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -14,27 +14,12 @@ class ImagesController < ApplicationController
   end
  
   def create
-    config.logger = Logger.new(STDOUT)
     commit = params[:commit]
-    logger.debug commit
     if commit == "Select Image"
-      image = params['image']
-      @file_name = image["name"]
-      @dir = File.dirname(@file_name)
-      @base = File.basename(@file_name)
-      @image_name  = @dir + "/" + "large_" + @base
-      logger.debug @image_name
-      url = { :controller => 'product', :action => 'index', :id => params["product_id"], :image_id => @image_name }
-      redirect_to url
+      select_image
    elsif commit == "Upload Image"
-      @image = Image.new(image_params)
-      if @image.save
-        url = { :controller => 'product', :action => 'index', :id => params["product_id"] }
-        redirect_to url
-      else
-        render :new
-      end
-    else
+      upload_image
+   else
       url = { :controller => 'product', :action => 'index', :id => params["product_id"] }
       redirect_to url
     end
@@ -60,4 +45,27 @@ class ImagesController < ApplicationController
   def set_image
     @image = Image.find(params[:id])
   end
+
+  private
+
+  def select_image
+      image = params['image']
+      @file_name = image["name"]
+      @dir = File.dirname(@file_name)
+      @base = File.basename(@file_name)
+      @image_name  = @dir + "/" + "large_" + @base
+      url = { :controller => 'product', :action => 'index', :id => params["product_id"], :image_id => @image_name }
+      redirect_to url
+  end
+
+  def upload_image
+      @image = Image.new(image_params)
+      if @image.save
+        url = { :controller => 'product', :action => 'index', :id => params["product_id"] }
+        redirect_to url
+      else
+        render :new
+      end
+  end
+
 end
--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,6 +2,21 @@ class ProductController < ApplicationController
   def index
     @images = Image.order('created_at DESC') if !@images
     @design = params['image_id']
+    @design_index = 0
+    if @design
+      @dir = File.dirname(@design)
+      @base = File.basename(@design)
+      if @base.start_with?("large_")
+        @base = @base[6, @base.length]
+      end
+      @name = @dir + "/" + @base
+      @images.each_with_index {|img,i|
+        if img.image.to_s == @name
+          @design_index = i
+          break
+        end
+      }
+    end
     @id = params['id'].to_i
     @product = nil
     content = open("https://api.printful.com/products").read
